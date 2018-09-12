bd1419e1ccb744b62440220b71b993057e85575b
YuriyValkiv <yurivalkiv@gmail.com>
2018-09-12 11:17:05 +0200

    spring boot files

---
+++ src/test/java/com/mvyv/unnamedmarket/UnnamedmarketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mvyv.unnamedmarket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UnnamedmarketApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
